cfd9974f6893df902a3c25c5f1090ff15b2e2076
l3d4 <laszlo.vajner@icloud.com>
2024-05-04 18:54:12 +0200

    20240504-prog1-labor4.c

---
+++ 20240504-prog1-labor4.c
@@ -0,0 +1,53 @@
+/* 
+PROGRAMOZÁSI NYELVEK 1 LABOR
+2024.05.04 TEOK II 108
+https://elearning.unideb.hu/pluginfile.php/165380/mod_resource/content/1/Gyak2.pdf
+Sztring kezelés 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/*
+6. Számjegyek duplikálása
+Írj egy double_digits nevű függvényt, mely visszaadja azt a sztringet, melyet a paraméterként megkapott sztring számjegyeinek megkettőzésével kapunk.
+*/
+
+char* double_digits(char* s) {
+    int hossz = strlen(s);
+    char* result = calloc(2*hossz,sizeof(char));
+    int idx = 0; // másolás során helyiérték nyomon követése az új tömbben
+
+    for (int i = 0; i<hossz; i++) {
+        if (isdigit(s[i])) {
+            result[idx++] = s[i];
+            result[idx++] = s[i];
+        } else {
+            result[idx++] = s[i];
+        }
+    }
+    result[idx] = '\0';
+    
+    return result;
+}
+
+void test_7f() {
+    FILE* f_in = fopen("feladat7.txt", "r");
+    
+    char* sor[300];
+
+    while(fgets(sor, 300, f_in) != NULL) {
+        int hossz = strlen(sor);
+        sor[hossz-1] = '\0';
+
+        
+    }
+}
+
+int main() {
+    test7n();
+
+    return 0;
+}
